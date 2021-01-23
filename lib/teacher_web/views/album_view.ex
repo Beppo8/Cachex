9fa464450006e45f241436113d3fcda4d1e63a5a
beppo <beppo@localhost.localdomain>
2021-01-22 20:26:14 -0600

    Cachex: album_view.ex

--- lib/teacher_web/views/album_view.ex
+++ lib/teacher_web/views/album_view.ex
@@ -4,11 +4,15 @@ defmodule TeacherWeb.AlbumView do
   alias Teacher.Records
 
   def category_name(category_id) do
-    {_, name} = Cachex.fetch(:category_cache, category_id, fn(key) ->
-      category = Records.get_category(key)
-      {:commit, category.name}
-    end)
-    name
+    with {_, name} = Cachex.fetch(:category_cache, category_id, &set_category_name/1) do
+      Cachex.expire(:category_cache, category_id, :timer.seconds(5))
+      name
+    end
+  end
+
+  defp set_category_name(category_id) do
+    category = Records.get_category(category_id)
+    {:commit, category.name}
   end
 
 end
